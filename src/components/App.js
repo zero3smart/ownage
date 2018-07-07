9e2c070b8e021788951be1a78a232f13c61534b0
zero2smart <fivewisdomzero@gmail.com>
2018-07-07 14:55:43 -0400

    changes for latest video

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ class App extends React.Component {
     render() {
         return (
             <div className='main-container'>
-              <Header />
+                <Header />
                 {this.props.children}
             </div>
         );
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import '../assets/stylesheets/components/HomePage.scss';
+import headerBanner1 from '../assets/images/header_banner.png';
+import headerBanner2 from '../assets/images/header_banner2.png';
+import appStoreBtn from '../assets/images/app_store_btn.png';
+import googlePlayBtn from '../assets/images/google_play_btn.png';
 
 class HomePage extends React.Component {
     constructor(props) {
@@ -9,6 +13,21 @@ class HomePage extends React.Component {
     render() {
         return (
             <div className='home-container'>
+                <div id='banner1'>
+                    <img src={headerBanner1} alt='Header Banner1' />
+                </div>
+                <div id='banner2'>
+                    <img src={headerBanner2} alt='Header Banner2' />
+                    <a href='#' id='apple-store-btn'>
+                        <img src={appStoreBtn} alt='App Store Button' />
+                    </a>
+                    <a href='#' id='google-play-btn'>
+                        <img src={googlePlayBtn} alt='Google Play Button' />
+                    </a>
+                </div>
+                <div id='latest-video'>
+                    <h1>Latest Videos</h1>
+                </div>
             </div>
         );
     }
