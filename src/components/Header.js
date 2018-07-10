e48fccc95b05d814e7908cff6d63a12f87f3102e
zero2smart <fivewisdomzero@gmail.com>
2018-07-09 22:50:40 -0400

    modify header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import '../assets/stylesheets/components/Header.scss';
 import NavigationBar from './NavigationBar';
+import headerBanner1 from '../assets/images/header_banner.png';
+import '../assets/stylesheets/components/Header.scss';
 
 class Header extends React.Component {
     render() {
         return (
             <div className='header-container'>
                 <NavigationBar />
+                <div id='banner1'>
+                    <img src={headerBanner1} alt='Header Banner1' />
+                </div>
             </div>
         );
     }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import LatestVideo from './LatestVideo';
 import '../assets/stylesheets/components/HomePage.scss';
-import headerBanner1 from '../assets/images/header_banner.png';
 import headerBanner2 from '../assets/images/header_banner2.png';
 import appStoreBtn from '../assets/images/app_store_btn.png';
 import googlePlayBtn from '../assets/images/google_play_btn.png';
@@ -14,9 +13,6 @@ class HomePage extends React.Component {
     render() {
         return (
             <div className='home-container'>
-                <div id='banner1'>
-                    <img src={headerBanner1} alt='Header Banner1' />
-                </div>
                 <div id='banner2'>
                     <img src={headerBanner2} alt='Header Banner2' />
                     <a href='#' id='apple-store-btn'>
